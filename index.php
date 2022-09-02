bf9d51e4a2ff8266ae5bc17705426299a7b89c22
Franpan94 <francescopanico94@libero.it>
2022-09-02 16:04:41 +0200

    initialization of a new instance

--- index.php
+++ index.php
@@ -1,40 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Film</h1>
-    <h4>
-        <?php 
-            class movie {
-                public $title;
-                public $genre;
-                public $plot;
+    <h3>
+      <?php
+        class movie
+        {
+            public $title;
+            public $genre;
+            public $plot;
+
+
+            function __construct($_title, $_genre, $_plot)
+            {
+                $this->title = $_title;
+                $this->genre = $_genre;
+                $this->plot = $_plot;
             }
 
-            function __construct($_title, $_genre, $_plot){
-                $this -> title = $_title;
-                $this -> genre = $_genre;
-                $this -> plot = $_plot;
+            public function getTitle()
+            {
+                return $this->title;
             }
 
-            public function getTitle(){
-                return $this -> title;
+            public function getgenre()
+            {
+                return $this->genre;
             }
 
-            public function getgenre(){
-                $this -> genre;
-            } 
+            public function getplot()
+            {
+                return $this->plot;
+            }
+        }
 
-            public function getplot(){
-                $this -> plot;
-            } 
-           $nuovofilm = new movie ('ciao', 'avventura', 'molto interessante')
-        ?>
-    </h4>
+        $movieone = new movie('ciao', 'avventura', 'molto interessante');
+        echo 'Film: ' . $movieone->getTitle() . '<br>';
+        echo 'Genere: ' . $movieone->getgenre() . '<br>';
+        echo 'Trama: ' . $movieone->getplot() . '<br>';
+        
+      ?>
+    </h3>
 </body>
+
 </html>
